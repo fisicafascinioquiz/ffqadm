6ffddd70b7d7b571c06c5a4c28aa2ae80c444810
fisicafascinioquiz <profguilhermebila@gmail.com>
2025-10-07 21:08:51 -0300

    Add files via upload

--- main.js
+++ main.js
@@ -89,7 +89,7 @@ export async function fetchSubcategories(categoryId) {
             card.innerHTML = `
                 <h3>${subcategory.subcategoryName}</h3>
                 <img 
-                    src="${subcategory.subcategoryImage}" 
+                    src="${formatDriveLink(subcategory.subcategoryImage)}" 
                     alt="${subcategory.subcategoryName}" 
                     class="subcategory-image"
                    onerror="this.onerror=null;this.src='https://placehold.co/120x120?text=Erro+na+Imagem';"
